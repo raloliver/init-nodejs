cbf0ffe02c686dcd6ee9597c6588d579cd7ac2d2
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-16 12:36:07 +0100

    Get pikachu

--- list/service.js
+++ list/service.js
@@ -1,1 +1,16 @@
 const axios = require('axios')
+const API_URI = `https://pokeapi.co/api/v2/pokemon/`
+
+async function getPoke(name) {
+    const url = `${API_URI}/${name}/`
+    const res = await axios.get(url)
+    return res.data
+}
+
+getPoke('pikachu')
+    .then(function (res) {
+        console.log('pokemon', res)
+    })
+    .catch(function (err) {
+        console.error('Error', err)
+    })
