bdfeb6bb2478073c45aebc04252ac4991234ef34
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-16 13:22:12 +0100

    Tests with for, for in and for of

--- list/for.js
+++ list/for.js
@@ -8,8 +8,21 @@ async function main() {
     try {
         const res = await service.getPoke('')
         const names = []
+        /**
+         * for 
         for (let i = 0; i < res.results.length; i++) {
             const pokes = res.results[i]
+            names.push(pokes.name)
+        }
+        */
+        /**
+         * for in  
+         for (const i in res.results) {
+             const pokes = res.results[i]
+             names.push(pokes.name)
+         }
+         */
+        for (const pokes of res.results) {
             names.push(pokes.name)
         }
         console.log('names', names)
