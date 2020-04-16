88d8c0df9b57ceeda0fe23960420fb2b2a38ce2f
Israel Oliveira <raloliver.com@gmail.com>
2020-04-16 19:40:17 +0100

    Use forEach

--- list/map.js
+++ list/map.js
@@ -3,13 +3,18 @@ const service = require('./service')
 async function main() {
   try {
     const res = await service.getPoke('')
-    const names = []
+
     /**
      * forEach implementation
-     */
+     * 
+    const names = []
     res.results.forEach(function (poke) {
       names.push(poke.name)
     });
+    */
+    const names = res.results.map(function (poke) {
+      return poke.name
+    })
     console.log(names)
   } catch (err) {
     console.log('Error ', err)
