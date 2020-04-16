700015b8e0788dea3ceea951119c7687bc9e737f
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-16 12:39:43 +0100

    Init with for in and for of on nodejs

--- list/service.js
+++ list/service.js
@@ -1,6 +1,10 @@
 const axios = require('axios')
 const API_URI = `https://pokeapi.co/api/v2/pokemon/`
 
+/**
+ * when whant pagination, use those params:     ?offset=0&limit=10
+ * @param {*} name 
+ */
 async function getPoke(name) {
     const url = `${API_URI}/${name}/`
     const res = await axios.get(url)
@@ -14,3 +18,5 @@ getPoke('pikachu')
     .catch(function (err) {
         console.error('Error', err)
     })
+
+module.exports = getPoke
